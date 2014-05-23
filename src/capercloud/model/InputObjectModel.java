254955f775942d5e9826004e751f66bae76d1b6f
shuai <shuai@void>
2014-05-23 12:13:53 +0800

    搜库配置文件上传到s3

---
+++ src/capercloud/model/InputObjectModel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package capercloud.model;
+
+import capercloud.JobOverviewController;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import org.jets3t.service.model.S3Object;
+
+/**
+ *
+ * @author shuai
+ */
+public class InputObjectModel {
+    private BooleanProperty selected;
+    private S3Object obj;
+    
+    public InputObjectModel(S3Object obj) {
+        this.obj = obj;
+        this.selected = new SimpleBooleanProperty(false);
+        this.selected.addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) {
+                if (t1) {
+                    String str = JobOverviewController.tfSelectedNumOfInputSpectra.getText();
+                    int num = Integer.parseInt(str);
+                    num = num + 1;
+                    JobOverviewController.tfSelectedNumOfInputSpectra.setText(Integer.toString(num));
+                }
+                if (t) {
+                    String str = JobOverviewController.tfSelectedNumOfInputSpectra.getText();
+                    int num = Integer.parseInt(str);
+                    num = num - 1;
+                    JobOverviewController.tfSelectedNumOfInputSpectra.setText(Integer.toString(num));
+                }
+            }
+        });
+    }
+
+    public void setSelected(SimpleBooleanProperty selected) {
+        this.selected = selected;
+    }
+
+    public BooleanProperty selectedProperty() {
+        return selected;
+    }
+
+    public S3Object getObj() {
+        return obj;
+    }
+
+    public void setObj(S3Object obj) {
+        this.obj = obj;
+    }
+    
+    public String getName() {
+        return this.obj.getName();
+    }
+}
