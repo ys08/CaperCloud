c5e74797428ac0de7f32ce16e957e7f7f95c71b7
shuai <yspku@qq.com>
2014-06-20 18:47:38 +0800

    wip

--- src/capercloud/model/ResultModel.java
+++ src/capercloud/model/ResultModel.java
@@ -8,6 +8,7 @@ package capercloud.model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -17,8 +18,15 @@ import javafx.embed.swing.SwingNode;
 import javafx.scene.layout.AnchorPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import uk.ac.ebi.jmzidml.MzIdentMLElement;
 import uk.ac.ebi.jmzidml.model.mzidml.AnalysisData;
+import uk.ac.ebi.jmzidml.model.mzidml.CvParam;
 import uk.ac.ebi.jmzidml.model.mzidml.DataCollection;
+import uk.ac.ebi.jmzidml.model.mzidml.Modification;
+import uk.ac.ebi.jmzidml.model.mzidml.Peptide;
+import uk.ac.ebi.jmzidml.model.mzidml.PeptideEvidence;
+import uk.ac.ebi.jmzidml.model.mzidml.PeptideEvidenceRef;
+import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationItem;
 import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationList;
 import uk.ac.ebi.jmzidml.model.mzidml.SpectrumIdentificationResult;
 import uk.ac.ebi.jmzidml.xml.io.MzIdentMLUnmarshaller;
@@ -38,19 +46,20 @@ public class ResultModel {
     
     private JMzReader jmzReader;
     private MzIdentMLUnmarshaller unmarshaller;
-    private ObservableList<SpectrumIdentificationResult> spectrumIdentificationResult;
+    private ObservableList<PeptideModel> peptideList;
+    private Map<String, ObservableList<SpectrumModel>> peptideToSpectrumMap;
     
-    SpectrumPanel spectrumPanel;
+    private SpectrumPanel spectrumPanel;
     
     public ResultModel() {
-        this.spectrumIdentificationResult = FXCollections.observableArrayList();
+        this.peptideList = FXCollections.observableArrayList();
+        this.peptideToSpectrumMap = new HashMap<>();
     }
     
     //getters and setters
-    
-    public ObservableList<SpectrumIdentificationResult> getSpectrumIdentificationResult() {
-        return spectrumIdentificationResult;
-    
+
+    public ObservableList<PeptideModel> getPeptideList() {
+        return peptideList;
     }
 
     public void load(File resultFile, File spectraFile) throws JMzReaderException {
@@ -59,27 +68,38 @@ public class ResultModel {
         
         this.jmzReader = new MgfFile(this.spectraFile);
         this.unmarshaller = new MzIdentMLUnmarshaller(this.resultFile);
+
+        Iterator<SpectrumIdentificationItem> siis = unmarshaller.unmarshalCollectionFromXpath(MzIdentMLElement.SpectrumIdentificationItem);
         
-        DataCollection dc =  unmarshaller.unmarshal(DataCollection.class);
-        AnalysisData ad = dc.getAnalysisData();
-        List<SpectrumIdentificationList> sil = ad.getSpectrumIdentificationList();
-        
-        for (SpectrumIdentificationList sIdentList : sil) {
-            for (SpectrumIdentificationResult spectrumIdentResult 
-                    : sIdentList.getSpectrumIdentificationResult()) {
-                this.spectrumIdentificationResult.add(spectrumIdentResult);
-//            // as MGF files are index based the "index=" portion of the spectrumId needs to be removed
-//            String spectrumIndex = spectrumID.substring(6);
-//
-//            //  and since the index in mzid files is 0 based and the index in jmzReader is 1 based, this has to be addressed as well
-//            int index = Integer.parseInt(spectrumIndex);
-//            index = index + 1;
-//            System.out.println(index);
-//            // using the index the spectrum can now be retrieved from the
-//            // MGF file.
-//            Spectrum spectrum = mgfParser.getSpectrumByIndex(index);
-            }
+        while (siis.hasNext()) {
+            SpectrumIdentificationItem sii = siis.next();
+            String peptideRef = sii.getPeptideRef();
+            
         }
+//        while (siis.hasNext()) {
+//            siis.next().getPeptideRef();
+//        }
+//        int count = 0;
+//        while (sii.hasNext()) {
+//            SpectrumIdentificationItem spectrumIdentItem = sii.next();
+//            
+//            if (!spectrumIdentItem.getPeptideEvidenceRef().isEmpty()) {
+//                count++;
+//                Peptide pep = spectrumIdentItem.getPeptide();
+//                List<Modification> mods = pep.getModification();
+//                StringBuilder sb = new StringBuilder();
+//                if (!mods.isEmpty()) {
+//                    for (Modification mod : mods) {
+//                        for (CvParam cp : mod.getCvParam()) {
+//                            sb.append(cp.getAccession()).append(",");
+//                        }
+//                    }
+//                }
+//                this.peptideList.add(new PeptideModel(String.valueOf(count), pep.getPeptideSequence(), 
+//                        spectrumIdentItem.getPeptideEvidenceRef().get(0).getPeptideEvidence().getDBSequenceRef(),
+//                        sb.toString()));
+//            }
+//        }
     }
     
     public void init(AnchorPane pane) {
