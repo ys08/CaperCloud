e58f0ed0e18587002068b3ee7af2c99d4bb258b7
shuai <yspku@qq.com>
2014-06-23 15:52:11 +0800

    wip

--- src/capercloud/model/PeptideModel.java
+++ src/capercloud/model/PeptideModel.java
@@ -8,39 +8,63 @@ package capercloud.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.collections.ObservableList;
 
 /**
  *
  * @author shuai
  */
 public class PeptideModel {
-    private StringProperty peptideId;
+    private String id;
+    
+    private StringProperty chrom;
     private StringProperty peptideSeq;
-    private StringProperty genomicLocation;
-    private StringProperty modification;
-    private ObservableList<SpectrumModel> psmList;
+    private StringProperty proteinStart;
+    private StringProperty proteinEnd;
+    private StringProperty peptideStart;
+    private StringProperty peptideEnd;
+    private StringProperty modifications;
     
-    public PeptideModel(String peptideId, String peptideSeq, String genomicLocation, String modification) {
-        this.peptideId = new SimpleStringProperty(peptideId);
+    public PeptideModel(String id, String chrom, String peptideSeq, String proteinStart, String proteinEnd, String peptideStart, String peptideEnd, String modifications) {
+        this.id = id;
+        this.chrom = new SimpleStringProperty(chrom);
         this.peptideSeq = new SimpleStringProperty(peptideSeq);
-        this.genomicLocation = new SimpleStringProperty(genomicLocation);
-        this.modification = new SimpleStringProperty(modification);
+        this.proteinStart = new SimpleStringProperty(proteinStart);
+        this.proteinEnd = new SimpleStringProperty(proteinEnd);
+        this.peptideStart = new SimpleStringProperty(peptideStart);
+        this.peptideEnd = new SimpleStringProperty(peptideEnd);
+        this.modifications = new SimpleStringProperty(modifications);
     }
-    
-    public StringProperty peptideIdProperty() {
-        return this.peptideId;
+
+    public String getId() {
+        return id;
     }
-    
+
+    public StringProperty chromProperty() {
+        return chrom;
+    }
+
     public StringProperty peptideSeqProperty() {
-        return this.peptideSeq;
+        return peptideSeq;
+    }
+
+    public StringProperty proteinStartProperty() {
+        return proteinStart;
+    }
+
+    public StringProperty proteinEndProperty() {
+        return proteinEnd;
+    }
+
+    public StringProperty peptideStartProperty() {
+        return peptideStart;
+    }
+
+    public StringProperty peptideEndProperty() {
+        return peptideEnd;
+    }
+
+    public StringProperty modificationsProperty() {
+        return modifications;
     }
     
-    public StringProperty genomicLocationProperty() {
-        return this.genomicLocation;
-    }
-    
-    public StringProperty modificationProperty() {
-        return this.modification;
-    }
 }
