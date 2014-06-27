c648e86f439979ea94bf037f4062b5bc3ffb2ce5
shuai <yspku@qq.com>
2014-06-27 16:02:48 +0800

    wip

--- src/capercloud/TypeOneController.java
+++ src/capercloud/TypeOneController.java
@@ -8,7 +8,12 @@ package capercloud;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -20,9 +25,26 @@ public class TypeOneController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    private ObservableList<String> chromNumList;
+    
+    @FXML ComboBox cbChromNum;
+    @FXML TextField tfFdr;
+    
+    public TypeOneController() {
+        this.chromNumList = FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        this.cbChromNum.setItems(chromNumList);
     }    
     
+    public String getSelectedChromosomeNumber() {
+        return (String) this.cbChromNum.getSelectionModel().getSelectedItem();
+    }
+    
+    public String getFdr() {
+        return this.tfFdr.getText();
+    }
 }
